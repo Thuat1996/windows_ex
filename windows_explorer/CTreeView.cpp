4f895b67975f0f7034bca262161a1edffd52eabe
thuat1996 <nguyenvanthuat96bn@gmail.com>
2018-09-22 22:37:01 +0700

    loadicontreeview

--- windows_explorer/CTreeView.cpp
+++ windows_explorer/CTreeView.cpp
@@ -1,30 +1,34 @@
 #include "stdafx.h"
 #include "CTreeView.h"
+//set image
+//int imageList[4];
 
 //cac bien duong dan
 extern TCHAR Path[MAX_PATH];
 extern TCHAR Path1[MAX_PATH];
 extern TCHAR Path2[MAX_PATH];
 extern TCHAR History[100][MAX_PATH];
+extern HINSTANCE hInst;
 TV_ITEM tvi;
 TV_INSERTSTRUCT tvinsert;
 HTREEITEM Selected = NULL;
 bool flagSelected = false;
 TCHAR szName[260] = _T("");
 TCHAR szTmp[260] = _T("");
+HWND hwndTV;
 CTreeView::CTreeView()
 {
 }
 CTreeView::~CTreeView()
 {
 }
-HWND CTreeView::CreateTV(HWND hWnd,HINSTANCE g_hInst)
-{
-	HWND hwndTV = CreateWindow(WC_TREEVIEW, L"Tree View", WS_CHILD | WS_VISIBLE | WS_BORDER | TVS_HASLINES | TVS_HASBUTTONS | TVS_LINESATROOT |
-		ES_AUTOHSCROLL | ES_AUTOVSCROLL | WS_HSCROLL | WS_VSCROLL | TVS_SHOWSELALWAYS, 0, 85,
-		300, 200, hWnd, (HMENU)IDC_MAIN_TREEVIEW, g_hInst, NULL);
-	return hwndTV;
-}
+//HWND CTreeView::CreateTV(HWND hWnd,HINSTANCE g_hInst)
+//{
+//	hwndTV = CreateWindow(WC_TREEVIEW, L"Tree View", WS_CHILD | WS_VISIBLE | WS_BORDER | TVS_HASLINES | TVS_HASBUTTONS
+//		| TVS_SINGLEEXPAND, 0, 85,
+//		300, 200, hWnd, (HMENU)IDC_MAIN_TREEVIEW, g_hInst, NULL);
+//	return hwndTV;
+//}
 void CTreeView::size(HWND hWnd)
 {
 	RECT rect;
@@ -36,6 +40,7 @@ void CTreeView::size(HWND hWnd)
 		/*load o dia tu dong*/
 void CTreeView::LoadDisk(HWND hWnd)
 {
+	
 	DWORD uDriveMask = GetLogicalDrives(); // ham load o dia /*ham tra ve 1 bitmask voi o A la 0, b=1....*/
 	TCHAR szDriver[] = _T("A");
 	while (uDriveMask)
@@ -58,7 +63,6 @@ void CTreeView::LoadFolder(HWND hWnd, TCHAR *szS, HTREEITEM Parent)
 	WIN32_FIND_DATA fd;
 
 	swprintf_s(szFolder, _T("%s/*.*"),szS);
-
 	hFind = FindFirstFile(szFolder, &fd);
 	if (hFind != INVALID_HANDLE_VALUE)
 	{
@@ -78,20 +82,32 @@ void CTreeView::LoadFolder(HWND hWnd, TCHAR *szS, HTREEITEM Parent)
 		FindClose(hFind);
 	}
 }
+//add this PC
+//void CTreeView::LoadPC(HWND hWnd, TCHAR *lpszItem)
+//{
+//	tvinsert.item.mask = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE | TVIF_PARAM;
+//	tvinsert.hParent = NULL;
+//	tvinsert.hInsertAfter = TVI_ROOT;
+//	tvinsert.item.iImage = imageList[IDB_PC];
+//	tvinsert.item.iSelectedImage = imageList[IDB_PC];
+//	tvinsert.item.pszText = lpszItem;
+//	tvinsert.item.lParam = (LPARAM)_T("This PC");
+//	HTREEITEM hThisPC = TreeView_InsertItem(hwndTV, &tvinsert);
+//}
 void CTreeView::AddRoot(HWND hWnd, TCHAR *szS)
 {
-	tvinsert.hInsertAfter = TVI_ROOT;
+	tvinsert.hInsertAfter = TVI_LAST;
 	tvinsert.hParent = NULL;
 	tvinsert.item.mask = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE;
 	tvinsert.item.pszText = szS;
-	tvinsert.item.iImage = 3;
-	tvinsert.item.iSelectedImage = 3;
+	tvinsert.item.iImage = 0;
+	tvinsert.item.iSelectedImage = 0;
 	SendDlgItemMessage(hWnd, IDC_MAIN_TREEVIEW, TVM_INSERTITEM, 0, (LPARAM)&tvinsert);
 }
 void CTreeView::AddNote(HWND hWnd, TCHAR* szText, HTREEITEM Parent)
 {
 	tvinsert.item.mask = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE;
-	tvinsert.item.iImage = 0;
+	tvinsert.item.iImage = 2;
 	tvinsert.item.iSelectedImage = 1;
 	tvinsert.hParent = Parent;
 	tvinsert.hInsertAfter = TVI_LAST;
@@ -100,6 +116,7 @@ void CTreeView::AddNote(HWND hWnd, TCHAR* szText, HTREEITEM Parent)
 }
 void CTreeView::onSelChange(HWND hWnd)
 {
+	//setImage(hwndTV);
 	int wIndex = 0;
 	HTREEITEM save;
 	bool check = FALSE;
@@ -122,7 +139,7 @@ void CTreeView::onSelChange(HWND hWnd)
 	//lay duong dan
 	while (Selected != NULL)
 	{
-		TCHAR szCpy[1000];
+		TCHAR szCpy[MAX_PATH];
 		tvi.hItem = Selected;
 		SendDlgItemMessage(hWnd, IDC_MAIN_TREEVIEW, TVM_GETITEM, TVGN_CARET, (LPARAM)&tvi);
 		swprintf_s(szCpy, _T("%s/%s"), tvi.pszText, Path);
@@ -134,6 +151,30 @@ void CTreeView::onSelChange(HWND hWnd)
 	}
 	//duyet folder
 	LoadFolder(hWnd, Path, save);
+}
+//set image cho treeview
+void CTreeView::setImage(HWND hWnd)
+{
+	HIMAGELIST hImageList;
+	HICON hIcon;
+	hImageList = ImageList_Create(16, 16, ILC_COLORDDB | ILC_MASK, 2, 2);
+	hIcon = (HICON)LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(IDI_DISK), IMAGE_ICON,
+		16, 16, LR_DEFAULTCOLOR);
+	ImageList_AddIcon(hImageList, hIcon);   // Macro: Attach the image, to the image list
 
+	hIcon = (HICON)LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(IDI_FOLOPEN), IMAGE_ICON, 
+		16, 16, LR_DEFAULTCOLOR);
+	ImageList_AddIcon(hImageList, hIcon);
+
+	hIcon = (HICON)LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(IDI_FOLCLOSE), IMAGE_ICON,
+		16, 16, LR_DEFAULTCOLOR);
+	ImageList_AddIcon(hImageList, hIcon);
+
+	hIcon = (HICON)LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(IDI_FILE), IMAGE_ICON,
+		16, 16, LR_DEFAULTCOLOR);
+	ImageList_AddIcon(hImageList, hIcon);   // Macro: Attach the image, to the image list
+	SendMessage(hWnd, TVM_SETIMAGELIST, (WPARAM)TVSIL_NORMAL, (LPARAM)hImageList);
+	
+	//TreeView_SetImageList(hwndTV, himl, TVSIL_NORMAL);
 }
 
--- windows_explorer/CTreeView.h
+++ windows_explorer/CTreeView.h
@@ -2,6 +2,10 @@
 #include "stdafx.h"
 #include "windows_explorer.h"
 #include "commctrl.h"
+//#define DRIVE_IM 0
+//#define FOLDER_IM 1
+//#define FILE 2
+//#define CDROM 3
 #define IDC_MAIN_TREEVIEW 4
 class CTreeView
 {
@@ -23,5 +27,6 @@ public:
 	void AddRoot(HWND, TCHAR*); // them nut cha
 	void AddNote(HWND, TCHAR*, HTREEITEM);// them nut con
 	void getPath(HWND, HTREEITEM, TCHAR); // ham lay duong da
+	void setImage(HWND);
 };
 
